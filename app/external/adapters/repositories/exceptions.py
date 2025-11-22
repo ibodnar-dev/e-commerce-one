ed39edcacee1362786585360f70e89976d947c4b
ivan <ibodnar.dev@gmail.com>
2025-11-22 14:58:40 -0300

    Remove unused `DBError` class from repository exceptions

--- app/external/adapters/repositories/exceptions.py
+++ app/external/adapters/repositories/exceptions.py
@@ -3,7 +3,3 @@ from app.external.exceptions import ExternalError
 
 class RepositoryError(ExternalError):
     """Base repository exception."""
-
-
-class DBError(RepositoryError):
-    """Raised when a database operation fails."""
